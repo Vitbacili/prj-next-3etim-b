9beeb83a59e24145c6beb66788a8ee3eb96f76eb
Vitbacili <135022354+Vitbacili@users.noreply.github.com>
2024-06-10 11:11:39 -0300

    TCC - 10/06

--- src/app/temp/page.jsx
+++ src/app/temp/page.jsx
@@ -6,8 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <h1>Links</h1>
-      <Link  href={"/plano"}><p className={styles.linkTemp}>Listar Planos</p></Link>
-      <Link  href={"/treino"}><p className={styles.linkTemp}>Listar Treinos</p></Link>
+      <Link  href={"/exercicio/cadastrar"}><p className={styles.linkTemp}>Cadastrar Exercício</p></Link>
+      <Link  href={"/exercicio/listar"}><p className={styles.linkTemp}>Listar Exercício</p></Link>
+      <Link  href={"/plano/cadastro"}><p className={styles.linkTemp}>Cadastrar Planos</p></Link>
+      <Link  href={"/plano/listar"}><p className={styles.linkTemp}>Listar Planos</p></Link>
+      <Link  href={"/treino/RecomendarTreino"}><p className={styles.linkTemp}>Recomendações de Treinos</p></Link>
+      <Link  href={"/treino/listar"}><p className={styles.linkTemp}>Listar Treinos</p></Link>
+      <Link  href={"/treino/InfoTreino"}><p className={styles.linkTemp}>Informações dos  Treinos</p></Link>
+      <Link  href={"/treino/cadastro"}><p className={styles.linkTemp}>Cadastrar Treinos</p></Link>
       <Link  href={"/usuarios/cadastrar"}><p className={styles.linkTemp}>Cadastrar usuarios</p></Link>
       <Link  href={"/usuarios/listar"}><p className={styles.linkTemp}>Listar usuarios</p></Link>
       <Link  href={"/usuarios/login"}><p className={styles.linkTemp}>Login</p></Link>
